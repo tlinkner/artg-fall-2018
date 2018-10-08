8b609db82a9ba1b49700fee8da752e9b9f2fb6ee
tlinkner <todd@toddlinkner.com>
2018-10-08 16:17:08 -0400

    week 5 work

--- week-05-01/script.js
+++ week-05-01/script.js
@@ -16,7 +16,8 @@ const arr_obj = Array.from({length:100}).map(function(){
 console.group('1.0');
 console.log(arr_num);
 console.log('arr_num has ' + arr_num.length + ' elements');
-/* YOUR CODE HERE: do the same for arr_obj */
+console.log(arr_obj);
+console.log('arr_obj has ' + arr_obj.length + ' elements');
 console.groupEnd();
 
 
@@ -33,15 +34,21 @@ arr_num.forEach(function(d, i){
 
 /* For arr_obj, print out each element */
 arr_obj.forEach(function(d, i){
-	/* YOUR CODE HERE */
+	console.log(`The ${i}th element in arr_obj is ${d}`);
 });
 
 /* A slight twist: for arr_obj, print out ONLY the .value property */
-/* YOUR CODE HERE */
+arr_obj.forEach(function(d, i){
+	console.log(d.value);
+});
 
 
 /* Bonus: can you use arr_obj.forEach to add up ALL OF THE VALUES? */
-
+let total = 0;
+arr_obj.forEach(function(d, i){
+    total += d.value;
+});
+console.log(`The total is ${total}`);
 console.groupEnd();
 
 
@@ -64,10 +71,12 @@ const arr_obj_half = arr_obj.map(function(d){
 });
 
 //Can you transform arr_obj into JUST an array of the values?
-/* YOUR CODE HERE */
+const arr_obj_vals = arr_obj.map(function(d){
+	return d.value;
+});
+console.log(arr_obj_vals);
 
 console.groupEnd();
-
 
 
 //1.3 array.sort
@@ -82,13 +91,17 @@ const arr_num_sorted_descending = arr_num.sort(function(a,b){
 	return b - a;
 });
 
+let myarr = ['c','d','b','e','a','f'];
+
+console.log(myarr.sort());
+
 //Can you sort arr_obj based on the .value property?
 const arr_obj_ascending = arr_obj.sort(function(a,b){
 	return a.value - b.value;
 });
 
 const arr_obj_descending = arr_obj.sort(function(a,b){
-	/* YOUR CODE HERE */
+	return b.value - a.value;
 });
 
 console.groupEnd();
@@ -105,8 +118,11 @@ const arr_num_filtered = arr_num.filter(function(d){
 
 //following the above example, can you produce a filtered array from arr_obj
 //containing only those elements with class == 'foo'?
-/* YOUR CODE HERE */
+const arr_num_foo = arr_obj.filter(function(d){
+	return d.class === 'foo';
+});
 
+console.log(arr_num_foo);
 
 //2.1 d3.min
 console.group('2.1');
@@ -124,25 +140,40 @@ console.groupEnd();
 //2.2 d3.max
 
 //Use d3.max to find the largest values in the arrays
-/* YOUR CODE HERE */
+const arr_num_max = d3.max(arr_num);
+const arr_obj_max = d3.max(arr_obj, function(d){
+	return d.value;
+});
 
 
 //2.3 d3.extent
 
 //Use d3.extent to find the min-max range in the arrays
-/* YOUR CODE HERE */
+const arr_num_ext = d3.extent(arr_num);
+console.log('arr_num_ext = ' + arr_num_ext);
+const arr_obj_ext = d3.extent(arr_obj, function(d){
+	return d.value;
+});
+console.log('arr_obj_ext = ' + arr_obj_ext);
 
 
 //2.4 d3.mean
 
 //Use d3.mean to find the average of the arrays
-/* YOUR CODE HERE */
+const arr_num_mean = d3.mean(arr_num);
+console.log('arr_num_mean = ' + arr_num_mean);
+const arr_obj_mean = d3.mean(arr_obj, function(d){
+	return d.value;
+});
+console.log('arr_obj_mean = ' + arr_obj_mean);
 
 
 //2.4 d3.nest
 const arr_obj_by_class = d3.nest()
 	.key(function(d){ return d.class })
 	.entries(arr_obj);
+    
+// This is pretty useful, but class is redundant 
 
 console.group('2.5');
 console.log(arr_obj_by_class);
--- week-05/script.js
+++ week-05/script.js
@@ -8,14 +8,14 @@
 function parse(d){
 	return {
 		/* YOUR CODE HERE: complete the parse function, making sure the parsed data has the following fields: */
-		//borough
-		//community board
-		//address: combining house number and street name
-		//lngLat: array of longitude and latitude as numbers
-		//job number
-		//permit type
-		//cost estimate: make sure it's imported as a number
-		//permit issuance date: make sure it's imported as a Date object
+		borough: d.borough,
+		community_board: d.community_board,
+		address: `${d.job_location_house_number} ${d.job_location_street_name}`, 
+        latlong: [d.latitude, d.longitude],
+		job_number: d.job_number,
+		permit_type: d.permit_type,
+		cost_estimate: d.cost_estimate, 
+		permit_issuance_date: new Date(d.permit_issuance_date),
 		job_type: d.job_type,
 		square_footage: +d.square_footage,
 	}
@@ -29,77 +29,107 @@ d3.csv(
 		parse //parse (function)
 	)
 	.then(function(data){
-		
 		//Question 2: how many records are there? Expect a single number
 		//Also, for each record, what attributes/fields are available?
 		//Hint: the variable "data" is an array, and array.length gives the number of elements in the array
-		const NUM_RECORDS = 0 /* YOUR CODE HERE */;
+		const NUM_RECORDS = data.length;
 		console.groupCollapsed('Question 2');
 		console.log(`There are ${NUM_RECORDS} records in the dataset`);
 		console.log('For each record, the attributes are as follows');
-		console.log(/* YOUR CODE HERE */);
+		console.log(Object.keys(data[0]));
 		console.groupEnd();
+        console.log(data[0]);
 
 
 		//Question 3: for each record, print its address in console
 		//Hint: use array.forEach
 		console.groupCollapsed('Question 3'); 
-		/* YOUR CODE HERE */
+		data.forEach(function(el){
+            console.log(el.address);
+        });
 		console.groupEnd();
+
 
 		//Question 4.1: can you transform "data" array into an array of just cost estimates?
 		//Hint: use array.map
-		const costEstimates = [] /* YOUR CODE HERE */;
+		const costEstimates = data.map(function(el){
+            return el.cost_estimate;
+        });
 		console.groupCollapsed('Question 4.1');
 		console.log(costEstimates);
 		console.groupEnd();
 
+
 		//Question 4.2: smallest cost estimate and largest cost estimate?
-		const minCostEstimate = undefined /* YOUR CODE HERE */;
-		const maxCostEstimate = undefined /* YOUR CODE HERE */;
+		const minCostEstimate = d3.min(costEstimates);
+		const maxCostEstimate = d3.max(costEstimates);
 		console.groupCollapsed('Question 4.2');
 		console.log(`Min and max cost estimates are ${minCostEstimate}, ${maxCostEstimate}`);
 		console.groupEnd();
 
+
 		//Question 4.3: it appears that some cost estimates are $1, which are probably trivial or placeholder estimates
 		//let's filter these out
-		const filteredCostEstimates = [] /* YOUR CODE HERE */;
+		const filteredCostEstimates = costEstimates.filter(function(el) {
+            return el > 1;
+        });
 		console.groupCollapsed('Question 4.3');
 		console.log(filteredCostEstimates);
+        // double check
+		console.log(d3.min(filteredCostEstimates));
 		console.groupEnd();
+
 
 		//Question 4.4: how many non-trivial cost estimates are there? What is the average cost estimate?
 		//Use the result from 4.3 directly
-		const averageCost = undefined /* YOUR CODE HERE */;
+		const averageCost = d3.sum(filteredCostEstimates) / filteredCostEstimates.length;
 		console.groupCollapsed('Question 4.4');
 		console.log(`There are ${filteredCostEstimates.length} non-trivial cost estiamtes`);
 		console.log(`Average cost estimate is ${averageCost}`);
 		console.groupEnd();
 
+
 		//Question 4.5: similarly, what percentage of the estimates are between $1 and $1,000,000
 		//What percentage are between $1,000,000 and $5,000,000
 		//And what percentage are over $5,000,000?
 		//Hint: use array.filter to narrow down all the records, then count them up and divide by the number of total records
-		/* YOUR CODE HERE */
+		const filteredRange = costEstimates.filter(function(el) {
+            if (el > 1000000 && el < 5000000) {
+                return el;
+            }
+        });
+        const rangePercent = filteredRange.length / filteredCostEstimates.length;
 		console.groupCollapsed('Question 4.5');
-		console.log(`${undefined}% of permits are between $1 and $1M`);
+		console.log(`${rangePercent}% of permits are between $1 and $1M`);
 		console.groupEnd();
 
-		//The following questions explore the categorical dimensions
+
+        //The following questions explore the categorical dimensions
+
 
 		//Question 5.1: let's look at one of the categorical dimensions (permit type)
 		//can you group all the permit records by the permit type attribute?
 		//Hint: use d3.nest()
-		const permitsByType = [] /* YOUR CODE HERE */; 
+		const permitsByType = d3.nest()
+            .key(function(d){ 
+                return d.permit_type 
+            })
+	       .entries(data);
 		console.groupCollapsed('Question 5.1');
 		console.log(permitsByType);
 		console.groupEnd();
 
+
 		//Question 5.2: similarly, can you group all the permit records by borough?
-		const permitsByBorough = [] /* YOUR CODE HERE */;
+		const permitsByBorough = d3.nest()
+            .key(function(d){ 
+                return d.borough 
+            })
+	       .entries(data);
 		console.groupCollapsed('Question 5.2');
 		console.log(permitsByBorough);
 		console.groupEnd();
+
 
 		//Question 5.3: all construction permits generally of higher value in some boroughs?
 		//To find out, let's take the result from 5.2, and compute the average (mean) cost estimate of all permits for each borough
@@ -108,18 +138,84 @@ d3.csv(
 		//Hint: from question 5.2, we already have a 5-element array of objects that represent each borough
 		//from here, we need to manipulate each element
 		//As discussed before, per-element array manipulation involves using array.map
-		const meanCostByBorough = undefined /* YOUR CODE HERE */;
+
+        permitsByBorough.map(function(d){
+            d.avg = d3.mean(d.values, function(d) {
+                return d.cost_estimate;
+            })
+        });
+        
+        permitsByBorough.sort(function(a,b){
+            return a.avg - b.avg;
+        });
+
 		console.groupCollapsed('Question 5.3');
-		console.log(meanCostByBorough);
-		console.groupEnd();
+            console.log(permitsByBorough);
+        console.groupEnd();
 
 		//Some additional questions to consider. See if you can manipulate the data to acquire these results:
 
-		//Is permit cost per square foot higher for new construction than for enlargements?
+		// Is permit cost per square foot higher for new construction than for enlargements?
 
-		//What is the time span of the dataset? What is the oldest permit? Newest?
+        filteredType = data.filter(function(el){
+            if (el.cost_estimate > 1 && el.square_footage > 1) { //  this was the tricky part: square_footage of 0
+                return el;
+            }
+        });
+
+		const filteredByType = d3.nest()
+            .key(function(d){ 
+                return d.permit_type 
+            })
+	       .entries(filteredType);
+
+        filteredByType.map(function(d){
+            d.values.map(function(d){
+                d.cost_sqft = d.cost_estimate / d.square_footage;
+            });
+        });
+        
+		console.groupCollapsed('Cost per Sq. Ft.');
+            filteredByType.map(function(d){
+                d.values.map(function(d){
+                    d.cost_sqft = d.cost_estimate / d.square_footage;
+                });
+                d.avg_cost_sqft = d3.mean(d.values, function(d) {
+                    return d.cost_sqft;
+                })
+            });
+            console.log(filteredByType);
+        console.groupEnd();
+
+        // What is the time span of the dataset? What is the oldest permit? Newest?
+
+		console.groupCollapsed('Oldest permit');
+            let oldestPermit = d3.min(data, function(d){
+                return d.permit_issuance_date;
+            });
+            console.log(oldestPermit);
+        console.groupEnd();
 
 		//Are there duplicate records at the same address? What does that mean?
+        
+        const dupes = data;
+        
+        dupes.sort(function(a,b){
+            return a.address - b.address;
+        });
+        
+        let dupeCount = 0;
+        
+        for (let i=0; i<dupes.length; i++) {
+            if (i > 0) {
+                if (dupes[i].address == dupes[i-1].address) {
+                    dupeCount += 1;
+                }
+            }
+        }
+		console.groupCollapsed('Duplicates');
+        console.log(`There are ${dupeCount} duplicates`)
+        console.groupEnd();
 
 	});
 
